4d414b86be4a45dc75ba062bafb458061facf12d
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-02 11:16:20 +0200

    Api Platform Part 1 - Chapter 19

--- symfony-casts/api-platform-part-1/src/Entity/CheeseListing.php
+++ symfony-casts/api-platform-part-1/src/Entity/CheeseListing.php
@@ -83,6 +83,13 @@ class CheeseListing
      */
     private $isPublished = false;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="cheeseListings")
+     * @ORM\JoinColumn(nullable=true)
+     * @Groups({"cheese_listing:read", "cheese_listing:write"})
+     */
+    private $owner;
+
     public function __construct(string $title = null)
     {
         $this->title = $title;
@@ -171,6 +178,18 @@ class CheeseListing
     public function setIsPublished(bool $isPublished): self
     {
         $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): self
+    {
+        $this->owner = $owner;
 
         return $this;
     }

c7fd7171ae22482101ed8da93c706e5d8548ba54
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-04 16:40:22 +0200

    Api Platform Part 1 - Chapter 24

--- symfony-casts/api-platform-part-1/src/Entity/CheeseListing.php
+++ symfony-casts/api-platform-part-1/src/Entity/CheeseListing.php
@@ -49,7 +49,7 @@ class CheeseListing
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read"})
+     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read", "user:write"})
      * @Assert\NotBlank()
      * @Assert\Length(
      *     min=2,
@@ -70,7 +70,7 @@ class CheeseListing
      * The price of this delicious cheese, in cents.
      *
      * @ORM\Column(type="integer")
-     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read"})
+     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read", "user:write"})
      * @Assert\NotBlank()
      */
     private $price;
@@ -135,7 +135,7 @@ class CheeseListing
     /**
      * The description of the cheese as raw text.
      *
-     * @Groups("cheese_listing:write")
+     * @Groups("cheese_listing:write", "user:write")
      * @SerializedName("description")
      */
     public function setTextDescription(string $description): self

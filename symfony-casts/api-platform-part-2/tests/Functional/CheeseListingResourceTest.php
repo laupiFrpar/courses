5fbd11481cedf8100642ef86993c6c6beffb5bbe
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-05 11:07:48 +0200

    Api Platform Part 2 - Chapter 13

--- symfony-casts/api-platform-part-2/tests/Functional/CheeseListingResourceTest.php
+++ symfony-casts/api-platform-part-2/tests/Functional/CheeseListingResourceTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Functional;
 
 use App\ApiPlatform\Test\ApiTestCase;
+use App\Entity\User;
 
 class CheeseListingResourceTest extends ApiTestCase
 {
@@ -17,5 +18,27 @@ class CheeseListingResourceTest extends ApiTestCase
             'json' => [],
         ]);
         $this->assertResponseStatusCodeSame(401);
+
+        $user = new User();
+        $user
+            ->setEmail('cheeseplease@example.com')
+            ->setUsername('cheeseplease')
+            ->setPassword('$2y$13$J4AGI6sPukuTWazfnGlEfOvVNFZ0Zit.ovuFt7uArevQEg7rNUQke')
+        ;
+
+        $em = self::$container->get('doctrine')->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        $client->request('POST', '/login', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'email' => 'cheeseplease@example.com',
+                'password' => 'foo',
+            ],
+        ]);
+        $this->assertResponseStatusCodeSame(204);
     }
 }

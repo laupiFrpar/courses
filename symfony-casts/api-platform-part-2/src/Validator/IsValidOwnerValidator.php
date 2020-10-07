67b56afeb77276856a2e0764ba61b4239bac5b98
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-07 16:41:11 +0200

    Api Platform Part 2 - Chapter 35

--- symfony-casts/api-platform-part-2/src/Validator/IsValidOwnerValidator.php
+++ symfony-casts/api-platform-part-2/src/Validator/IsValidOwnerValidator.php
@@ -2,11 +2,19 @@
 
 namespace App\Validator;
 
+use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 class IsValidOwnerValidator extends ConstraintValidator
 {
+    private $security;
+
+    public function __construct(Security $security) {
+        $this->security = $security;
+    }
+
     public function validate($value, Constraint $constraint)
     {
         /* @var $constraint \App\Validator\IsValidOwner */
@@ -15,7 +23,25 @@ class IsValidOwnerValidator extends ConstraintValidator
             return;
         }
 
-        $this->context->buildViolation($constraint->message)
-            ->addViolation();
+        $user = $this->security->getUser();
+        if (!$user instanceof User) {
+            $this->context->buildViolation($constraint->anonymousMessage)
+                ->addViolation()
+            ;
+        }
+
+        if ($this->security->isGranted('ROLE_ADMIN')) {
+            return;
+        }
+
+        if (!$value instanceof User) {
+            throw new \InvalidArgumentException('@IsValidOwner constraint must be put on a property containing a User object');
+        }
+
+        if ($value->getId() !== $user->getId()) {
+            $this->context->buildViolation($constraint->message)
+                ->addViolation()
+            ;
+        }
     }
 }

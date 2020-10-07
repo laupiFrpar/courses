ab215f6c2a41d95503dbceab9f908c62e4dffb93
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-07 16:43:34 +0200

    Api Platform Part 2 - Chapter 36

--- symfony-casts/api-platform-part-2/src/Entity/CheeseListing.php
+++ symfony-casts/api-platform-part-2/src/Entity/CheeseListing.php
@@ -47,6 +47,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @ApiFilter(RangeFilter::class, properties={"price"})
  * @ApiFilter(PropertyFilter::class)
  * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
+ * @ORM\EntityListeners({"App\Doctrine\CheeseListingSetOwnerListener"})
  */
 class CheeseListing
 {
@@ -100,7 +101,6 @@ class CheeseListing
      * @ORM\JoinColumn(nullable=false)
      * @Groups({"cheese:read", "cheese:collection:post"})
      * @IsValidOwner()
-     * @Assert\NotBlank()
      */
     private $owner;
 

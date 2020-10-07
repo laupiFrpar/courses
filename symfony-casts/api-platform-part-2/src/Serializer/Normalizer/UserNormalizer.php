c317574705898a97257eeba49bb70bbe0d1ffb90
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-07 16:08:37 +0200

    Api Platform Part 2 - Chapter 29

---
+++ symfony-casts/api-platform-part-2/src/Serializer/Normalizer/UserNormalizer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Serializer\Normalizer;
+
+use App\Entity\User;
+use Symfony\Component\Serializer\Normalizer\CacheableSupportsMethodInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class UserNormalizer implements NormalizerInterface, CacheableSupportsMethodInterface
+{
+    private $normalizer;
+
+    public function __construct(ObjectNormalizer $normalizer)
+    {
+        $this->normalizer = $normalizer;
+    }
+
+    /**
+     * @param User $object
+     */
+    public function normalize($object, $format = null, array $context = array()): array
+    {
+        if ($this->userIsOwner($object)) {
+            $context['groups'][] = 'owner:read';
+        }
+        $data = $this->normalizer->normalize($object, $format, $context);
+
+        // Here: add, edit, or delete some data
+
+        return $data;
+    }
+
+    public function supportsNormalization($data, $format = null): bool
+    {
+        return $data instanceof User;
+    }
+
+    public function hasCacheableSupportsMethod(): bool
+    {
+        return true;
+    }
+
+    private function userIsOwner(User $user): bool
+    {
+        return mt_rand(0, 10) > 5;
+    }
+}

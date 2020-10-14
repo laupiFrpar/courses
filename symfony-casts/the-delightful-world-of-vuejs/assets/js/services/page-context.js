a5f56ac4a3a5febafec9d13b6d172069311eb03e
Pierre-Louis Launay <lopi@marinlaunay.fr>
2020-10-14 14:58:35 +0200

    The delightful world of vuejs - Chapter 32

---
+++ symfony-casts/the-delightful-world-of-vuejs/assets/js/services/page-context.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the current category ID that's set by the server.
+ *
+ * @returns {string|null}
+ */
+export function getCurrentCategoryId() {
+  return window.currentCategoryId;
+}

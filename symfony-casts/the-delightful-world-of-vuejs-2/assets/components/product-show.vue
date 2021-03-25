6c0981e7e39d7444d6d3077c0b33a6fccf21698f
Pierre-Louis Launay <lopi@marinlaunay.fr>
2021-03-25 23:15:46 +0100

    The delightful world of vuejs part 2 - Chapter 5

--- symfony-casts/the-delightful-world-of-vuejs-2/assets/components/product-show.vue
+++ symfony-casts/the-delightful-world-of-vuejs-2/assets/components/product-show.vue
@@ -1,17 +1,37 @@
 <template>
-  <div>
-    I'd ❤️ to see a Product {{ productId }} here!
+  <div v-if="product">
+    <loading v-if="loading" />
+    <h1>{{ product.name }}</h1>
   </div>
 </template>
 
 <script>
+import Loading from '@/components/loading';
+import { fetchOneProduct } from '@/services/products-service';
+
 export default {
   name: 'ProductShow',
+  components: {
+    Loading,
+  },
   props: {
     productId: {
       type: String,
       required: true,
     },
+  },
+  data() {
+    return {
+      product: null,
+      loading: true,
+    };
+  },
+  async created() {
+    try {
+      this.product = (await fetchOneProduct(this.productId)).data;
+    } finally {
+      this.loading = false;
+    }
   },
 };
 </script>

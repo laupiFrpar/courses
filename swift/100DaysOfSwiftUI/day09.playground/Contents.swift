f5223427cbe327679138041a20ebfa674b20adcb
Pierre-Louis Launay <pl.launay@creads.com>
2023-09-15 12:15:51 +0200

    100 days of SwiftUI - Day 09

---
+++ swift/100DaysOfSwiftUI/day09.playground/Contents.swift
@@ -0,0 +1,150 @@
+/**
+ * 100 days of SwiftUI - Day 9
+ * https://www.hackingwithswift.com/100/swiftui/9
+ */
+import UIKit
+
+// How to create and use closures
+// https://www.hackingwithswift.com/quick-start/beginners/how-to-create-and-use-closures
+func greetUser() {
+    print("Hi there!")
+}
+
+greetUser()
+
+var greetCopy = greetUser
+greetCopy()
+
+let sayHello = {
+    print("Hi there!")
+}
+
+sayHello()
+
+let sayHello2 = { (name: String) -> String in
+    "Hi \(name)!"
+}
+
+func getUserData(for id: Int) -> String {
+    if id == 1989 {
+        return "Taylor Swift"
+    } else {
+        return "Anonymous"
+    }
+}
+
+let data: (Int) -> String = getUserData
+let user = data(1989)
+print(user)
+sayHello2("Taylor")
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+func captainFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+}
+
+let captainFirstTeam = team.sorted(by: captainFirstSorted)
+print(captainFirstTeam)
+
+let captainFirstTeam2 = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+})
+print(captainFirstTeam2)
+
+// How to use trailing closures and shorthand syntax
+// https://www.hackingwithswift.com/quick-start/beginners/how-to-use-trailing-closures-and-shorthand-syntax
+let captainFirstTeam3 = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+})
+
+print(captainFirstTeam)
+
+let captainFirstTeam4 = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+    
+    return $0 < $1
+}
+print(captainFirstTeam4)
+
+let reverseTeam = team.sorted { $0 > $1 }
+print(reverseTeam)
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
+
+// How to accept functions as parameters
+// https://www.hackingwithswift.com/quick-start/beginners/how-to-accept-functions-as-parameters
+func makeArray(size: Int, using generator: () -> Int) -> [Int] {
+    var numbers = [Int]()
+
+    for _ in 0..<size {
+        let newNumber = generator()
+        numbers.append(newNumber)
+    }
+
+    return numbers
+}
+
+let rolls = makeArray(size: 50) {
+    Int.random(in: 1...20)
+}
+
+print(rolls)
+
+func generateNumber() -> Int {
+    Int.random(in: 1...20)
+}
+
+let newRolls = makeArray(size: 50, using: generateNumber)
+print(newRolls)
+
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+// Checkpoint 5
+// https://www.hackingwithswift.com/quick-start/beginners/checkpoint-5
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
+
+var oddLuckyNumbers = luckyNumbers
+    .filter { !$0.isMultiple(of: 2) }
+    .sorted()
+    .map { String($0) + " is a lucky number" }
+
+for luckyNumber in oddLuckyNumbers {
+    print(luckyNumber)
+}

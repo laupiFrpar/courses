cfae0229d3c0bc858313eada3f2e49d71f7a7672
Pierre-Louis Launay <pl.launay@creads.com>
2023-09-17 18:53:48 +0200

    100 days of SwiftUI - Day 19

---
+++ swift/100DaysOfSwiftUI/Challenge1/Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by Pierre-Louis Launay on 16/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

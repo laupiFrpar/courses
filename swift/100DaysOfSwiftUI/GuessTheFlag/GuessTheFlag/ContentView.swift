2177bb90ba2dbb4373479063ed386f54c8393209
Pierre-Louis Launay <pl.launay@creads.com>
2023-09-17 22:32:45 +0200

    100 days of SwifUI - Day 21

--- swift/100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ swift/100DaysOfSwiftUI/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -163,9 +163,9 @@ struct ContentView: View {
     
     // Showing alert messages
     // https://www.hackingwithswift.com/books/ios-swiftui/showing-alert-messages
-    @State private var showingAlert = false
-    
-    var body: some View {
+//    @State private var showingAlert = false
+//
+//    var body: some View {
 //        Button("Show Alert") {
 //            showingAlert = true
 //        }
@@ -181,14 +181,105 @@ struct ContentView: View {
 //            Button("Cancel", role: .cancel) { }
 //        }
 //        
-        Button("Show Alert") {
-            showingAlert = true
+//        Button("Show Alert") {
+//            showingAlert = true
+//        }
+//        .alert("Important message", isPresented: $showingAlert) {
+//            Button("OK", role: .cancel) { }
+//        } message: {
+//            Text("Please read this.")
+//        }
+//    }
+    
+    // Stacking up buttons
+    // https://www.hackingwithswift.com/books/ios-swiftui/stacking-up-buttons
+    // Showing the player’s score with an alert
+    // https://www.hackingwithswift.com/books/ios-swiftui/showing-the-players-score-with-an-alert
+    // Styling our flags
+    // https://www.hackingwithswift.com/books/ios-swiftui/styling-our-flags
+    // Upgrading our design
+    // https://www.hackingwithswift.com/books/ios-swiftui/upgrading-our-design
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    var body: some View {
+        ZStack {
+//            Color.blue
+//            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+//                .init(color: .blue, location: 0.3),
+//                .init(color: .red, location: 0.3),
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
+                .ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundColor(.white)
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundColor(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+//                            .foregroundColor(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            }
+            .padding()
         }
-        .alert("Important message", isPresented: $showingAlert) {
-            Button("OK", role: .cancel) { }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
         } message: {
-            Text("Please read this.")
+            Text("Your score is ??")
         }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 

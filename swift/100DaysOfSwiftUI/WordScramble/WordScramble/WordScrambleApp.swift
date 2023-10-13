c1e4803a057e6c85644e7df0dc32182ca349cc09
Pierre-Louis Launay <pl.launay@creads.com>
2023-10-13 18:30:52 +0200

    100 days of SwiftUI - Day 29

---
+++ swift/100DaysOfSwiftUI/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Pierre-Louis Launay on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

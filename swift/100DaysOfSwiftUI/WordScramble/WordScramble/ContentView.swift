6e3a05b6310b1ee2058fcd448a6ba2fce0125f15
Pierre-Louis Launay <pl.launay@creads.com>
2023-10-13 19:11:36 +0200

    100 days of SwiftUI - Day 30

--- swift/100DaysOfSwiftUI/WordScramble/WordScramble/ContentView.swift
+++ swift/100DaysOfSwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -90,31 +90,154 @@ struct ContentView: View {
     
     // Working with strings
     // https://www.hackingwithswift.com/books/ios-swiftui/working-with-strings
+//    var body: some View {
+//        Text("Hello, world!")
+//            .padding()
+//        let input = "a b c"
+//        let letters = input.components(separatedBy: " ")
+//        
+//        let input2 = """
+//                    a
+//                    b
+//                    c
+//                    """
+//        let letters2 = input2.components(separatedBy: "\n")
+//        
+//        let letter = letters.randomElement()
+//        
+//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+//        
+//        let word = "swift"
+//        let checker = UITextChecker()
+//        
+//        let range = NSRange(location: 0, length: word.utf16.count)
+//        
+//        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+//        
+//        let allGood = misspelledRange.location == NSNotFound
+//    }
+    
+    // Adding to a list of words
+    // https://www.hackingwithswift.com/books/ios-swiftui/adding-to-a-list-of-words
+    // Running code when our app launches
+    // https://www.hackingwithswift.com/books/ios-swiftui/running-code-when-our-app-launches
+    // Validating words with UITextChecker
+    // https://www.hackingwithswift.com/books/ios-swiftui/validating-words-with-uitextchecker
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        let input = "a b c"
-        let letters = input.components(separatedBy: " ")
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+//                        .textInputAutocapitalization(.never)
+                        .autocapitalization(.none)
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         
-        let input2 = """
-                    a
-                    b
-                    c
-                    """
-        let letters2 = input2.components(separatedBy: "\n")
+        // exit if the remaining string is empty
+        guard answer.count > 0 else { return }
         
-        let letter = letters.randomElement()
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
         
-        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that wod from '\(rootWord)'!")
+            return
+        }
         
-        let word = "swift"
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        
+        newWord = ""
+    }
+    
+    func startGame() {
+        // 1. Find the URL for start.txt in our app bundle
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            // 2. Load start.txt into a string
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                // 3. Split the string up into an array of strings, splitting on line breaks
+                let allWords = startWords.components(separatedBy: "\n")
+
+                // 4. Pick one random word, or use "silkworm" as a sensible default
+                rootWord = allWords.randomElement() ?? "silkworm"
+
+                // If we are here everything has worked, so we can exit
+                return
+            }
+        }
+
+        // If were are *here* then there was a problem – trigger a crash and report the error
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
         let checker = UITextChecker()
-        
         let range = NSRange(location: 0, length: word.utf16.count)
-        
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
         
-        let allGood = misspelledRange.location == NSNotFound
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
     }
 }
 

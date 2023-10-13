21b4ff7c94732e50fb602f7881a0d5d8ce75a5ca
Pierre-Louis Launay <pl.launay@creads.com>
2023-10-13 11:03:02 +0200

    100 days of SwiftUI - Day 25 solution

---
+++ swift/100DaysOfSwiftUI/Challenge2Solution/Challenge2Solution/Challenge2SolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge2SolutionApp.swift
+//  Challenge2Solution
+//
+//  Created by Pierre-Louis Launay on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Challenge2SolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/100DaysOfSwiftUI/Challenge2Solution/Challenge2Solution/ContentView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  Challenge2Solution
+//
+//  Created by Pierre-Louis Launay on 13/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let moves = ["✊🏻", "✋🏻", "✌🏻"]
+    
+    @State private var computerChoice = Int.random(in: 0..<3)
+    @State private var shouldWin = Bool.random()
+    @State private var score = 0
+    @State private var questionCount = 1
+    @State private var showingResults = false
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            Text("Computer has played...")
+                .font(.headline)
+            Text(moves[computerChoice])
+                .font(.system(size: 200))
+            
+            if shouldWin {
+                Text("Which one wins ?")
+                    .foregroundStyle(.green)
+                    .font(.title)
+            } else {
+                Text("Which one loses ?")
+                    .foregroundStyle(.red)
+                    .font(.title)
+            }
+            
+            HStack {
+                ForEach(0..<3) { number in
+                    Button(moves[number]) {
+                        play(choice: number)
+                    }
+                    .font(.system(size: 80))
+                }
+            }
+            
+            Spacer()
+            
+            Text("Score: \(score)")
+                .font(.subheadline)
+            
+            Spacer()
+        }
+        .alert("Game over", isPresented: $showingResults) {
+            Button("Play Again", action: reset)
+        } message: {
+            Text("Your score was \(score)")
+        }
+    }
+    
+    func play(choice: Int) {
+        let winningMoves = [1, 2, 0]
+        let didWin: Bool
+        
+        if shouldWin {
+            didWin = choice == winningMoves[computerChoice]
+        } else {
+            didWin = winningMoves[choice] == computerChoice
+        }
+        
+        if didWin {
+            score += 1
+        } else {
+            score -= 1
+        }
+        
+        if questionCount == 10 {
+            showingResults = true
+        } else {
+            computerChoice = Int.random(in: 0..<3)
+            shouldWin.toggle()
+            questionCount += 1
+        }
+    }
+    
+    func reset() {
+        computerChoice = Int.random(in: 0..<3)
+        shouldWin = Bool.random()
+        questionCount = 0
+        score = 0
+    }
+}
+
+#Preview {
+    ContentView()
+}

63e5ba36b3543e01f3b660f299af759c21ccdc58
Pierre-Louis Launay <pl.launay@creads.com>
2023-11-21 13:44:58 +0100

    100 days of SwiftUI - Day 36-37

---
+++ swift/100DaysOfSwiftUI/Project7/Project7/ContentView.swift
@@ -0,0 +1,76 @@
+//
+//  ContentView.swift
+//  Project7
+//
+//  Created by Pierre-Louis Launay on 17/11/2023.
+//
+
+import SwiftUI
+
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+@Observable
+class Expenses {
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+    }
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        Text(item.amount, format: .currency(code: "USD"))
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift/100DaysOfSwiftUI/Project7/Project7/Project7App.swift
@@ -0,0 +1,17 @@
+//
+//  Project7App.swift
+//  Project7
+//
+//  Created by Pierre-Louis Launay on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Project7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

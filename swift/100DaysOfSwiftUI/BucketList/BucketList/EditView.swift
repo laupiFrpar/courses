b4a00800c9ee663827e8720411001c7a11f2beba
Pierre-Louis Launay <pl.launay@creads.com>
2024-01-25 12:40:55 +0100

    100 days of SwiftUI - Day 71

--- swift/100DaysOfSwiftUI/BucketList/BucketList/EditView.swift
+++ swift/100DaysOfSwiftUI/BucketList/BucketList/EditView.swift
@@ -10,8 +10,14 @@ import SwiftUI
 struct EditView: View {
     @Environment(\.dismiss) var dismiss
     
-    @State private var name: String
     @State private var description: String
+    @State private var loadingState = LoadingState.loading
+    @State private var name: String
+    @State private var pages = [Page]()
+    
+    enum LoadingState {
+        case loading, loaded, failed
+    }
     
     var location: Location
     var onSave: (Location) -> Void
@@ -22,6 +28,23 @@ struct EditView: View {
                 Section {
                     TextField("Place name", text: $name)
                     TextField("Description", text: $description)
+                }
+                
+                Section("Nearby...") {
+                    switch loadingState {
+                    case .loading:
+                        Text("Loading")
+                    case .loaded:
+                        ForEach(pages, id: \.pageId) { page in
+                            Text(page.title)
+                                .font(.headline)
+                            + Text(": ") 
+                            + Text(page.description)
+                                .italic()
+                        }
+                    case .failed:
+                        Text("Please try again later.")
+                    }
                 }
             }
             .navigationTitle("Place details")
@@ -37,6 +60,9 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
         }
     }
     
@@ -46,6 +72,30 @@ struct EditView: View {
         
         _name = State(initialValue: location.name)
         _description = State(initialValue: location.description)
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // we got some data back!
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            // success - convert the array value to our pages array
+            pages = items.query.pages.values.sorted()
+            
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
+        }
     }
 }
 

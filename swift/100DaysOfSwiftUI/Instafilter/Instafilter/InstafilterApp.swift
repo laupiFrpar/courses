9804b591ea71c0e9885bae5e19bd1a49b7a689fb
Pierre-Louis Launay <pl.launay@creads.com>
2024-01-25 08:51:32 +0100

    100 days of SwiftUI - Day 62-63-64-65-66

---
+++ swift/100DaysOfSwiftUI/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Pierre-Louis Launay on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

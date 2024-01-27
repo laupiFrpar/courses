d0e5d020d6456ae39a9e248011381a24c6ab16ed
Pierre-Louis Launay <pl.launay@creads.com>
2024-01-27 10:20:22 +0100

    Hacking with MacOS - Project 5

---
+++ swift/hackingWithMacOS/MultiMap/MultiMap/Location.swift
@@ -0,0 +1,24 @@
+//
+//  Location.swift
+//  MultiMap
+//
+//  Created by Pierre-Louis Launay on 27/01/2024.
+//
+
+import MapKit
+import Foundation
+
+struct Location: Identifiable, Hashable {
+    let id = UUID()
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
---
+++ swift/hackingWithMacOS/MultiMap/MultiMap/MultiMapApp.swift
@@ -0,0 +1,18 @@
+//
+//  MultiMapApp.swift
+//  MultiMap
+//
+//  Created by Pierre-Louis Launay on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MultiMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+    }
+}

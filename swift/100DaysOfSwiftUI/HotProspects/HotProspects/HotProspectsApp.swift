6c4e16d1aba73de082dbf41b2fc729387b47c9e1
Pierre-Louis Launay <pl.launay@creads.com>
2024-01-27 10:51:52 +0100

    100 days of SwiftUI - Day 79-80-81-82

---
+++ swift/100DaysOfSwiftUI/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Pierre-Louis Launay on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift/100DaysOfSwiftUI/HotProspects/HotProspects/Prospect.swift
@@ -0,0 +1,23 @@
+//
+//  Prospect.swift
+//  HotProspects
+//
+//  Created by Pierre-Louis Launay on 27/01/2024.
+//
+
+import Foundation
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var emailAddress = ""
+    var isContacted = false
+}
+
+@MainActor class Prospects: ObservableObject {
+    @Published var people: [Prospect]
+    
+    init() {
+        self.people = []
+    }
+}

fdd0cbba7879ddb13124a504e3805c7e2bec2019
Pierre-Louis Launay <pl.launay@creads.com>
2024-01-28 23:37:37 +0100

    100 days of SwiftUI - Day 86-87-88

---
+++ swift/100DaysOfSwiftUI/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Pierre-Louis Launay on 28/01/2024.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor who?", answer: "Jodie Whittaker")
+}

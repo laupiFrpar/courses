fbf3bc8ceb2afcfa29db2b0eb29c8e7155675f05
Pierre-Louis Launay <pl.launay@creads.com>
2024-01-30 21:01:03 +0100

    100 days of SwiftUI - Day 92-93-94-95-96-97-98-99

---
+++ swift/100DaysOfSwiftUI/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Pierre-Louis Launay on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
